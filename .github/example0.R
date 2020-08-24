629ccecb5e14697d89894a9975266b2026cf499a
Kirill Müller <krlmlr+r@mailbox.org>
2020-08-24 09:15:57 +0200

    Add example workflow from winch

---
+++ .github/example0.R
@@ -0,0 +1,17 @@
+library(winch)
+
+foo0 <- function() {
+  winch_call(bar0)
+}
+
+bar0 <- function() {
+  winch_call(baz0)
+}
+
+baz0 <- function() {
+  winch_trace_back()
+}
+
+winch_get_proc_map()
+
+foo0()
