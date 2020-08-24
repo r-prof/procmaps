0f5715a0d883cdbda0d3e856dec5ac69ea65b7b2
Kirill Müller <krlmlr+r@mailbox.org>
2020-08-24 09:16:40 +0200

    Adapt

--- .github/example0.R
+++ .github/example0.R
@@ -1,17 +1,3 @@
-library(winch)
+library(procmaps)
 
-foo0 <- function() {
-  winch_call(bar0)
-}
-
-bar0 <- function() {
-  winch_call(baz0)
-}
-
-baz0 <- function() {
-  winch_trace_back()
-}
-
-winch_get_proc_map()
-
-foo0()
+procmap_get()
