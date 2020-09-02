ab043d551305fd503bc57b1dea2adf6cd6694089
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-02 10:30:13 +0200

    Import

--- R/get.R
+++ R/get.R
@@ -12,7 +12,7 @@
 procmap_get <- function(as_tibble = NULL) {
   lines <- .Call(procmaps_c_procmap_get)
 
-  data <- read.delim(text = lines, sep = "\x1f", header = FALSE)
+  data <- utils::read.delim(text = lines, sep = "\x1f", header = FALSE)
   # https://stackoverflow.com/a/1401595/946850
   names(data) <- c("from", "to", "perms", "offset", "dev", "inode", "pathname")
 
