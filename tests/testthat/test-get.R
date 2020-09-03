857178188eccfeeea9b90dd792ebf77a75fec74d
Kirill Müller <krlmlr+r@mailbox.org>
2020-09-03 05:29:06 +0200

    Test validity of pathname component

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -10,3 +10,13 @@ test_that("column data types", {
   top_class <- vapply(classes, `[[`, 1, FUN.VALUE = character(1))
   expect_equal(top_class, rep("character", length(top_class)))
 })
+
+test_that("files exist", {
+  paths <- procmap_get(as_tibble = TRUE)$pathname
+  paths <- paths[paths != ""]
+  paths <- paths[!grepl("^[[]", paths)]
+  expect_equal(file.exists(paths), rep(TRUE, length(paths)))
+
+  lib_r <- grep("/libR[.]", paths)
+  expect_gt(length(lib_r), 0)
+})
